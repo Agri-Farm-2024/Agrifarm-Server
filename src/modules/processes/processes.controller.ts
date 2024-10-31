8ee904c3675c33edee89cc29c79089c54258ad2d
HanTG <trinhhan255@gmail.com>
2024-10-31 17:45:39 +0700

    fix requestModel

--- src/modules/processes/processes.controller.ts
+++ src/modules/processes/processes.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, UseGuards } from '@nestjs/common';
 import { ProcessesService } from './processes.service';
 import { CreateProcessDto } from './dto/create-process.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/common/guards/auth.guard';
 
 @ApiTags('Process')
 @Controller('processes')
 export class ProcessesController {
   constructor(private readonly processesService: ProcessesService) {}
 
+  
   @Post('/createProcessStandard')
   createProcessStandard(@Body() data: CreateProcessDto) {
     return this.processesService.createProcessStandard(data);
--- src/modules/requests/types/request-type.enum.ts
+++ src/modules/requests/types/request-type.enum.ts
@@ -2,4 +2,7 @@ export enum RequestType {
   new_account = 'new_account',
   view_land = 'view_land',
   product_purchase = 'product_purchase',
+  product_puchase_harvest = 'product_puchase_harvest',
+  technical_support = 'technical_support',
+  process_standard = 'process_standard',
 }
