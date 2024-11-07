2208046204a59014c8be58f902419032d7d9dc45
Huỳnh Chí Bảo <baohc110902@gmail.com>
2024-11-07 13:12:06 +0700

    feat: enhance CreateServiceSpecificDTO with ApiProperty decorators for improved Swagger documentation

--- src/modules/servicesPackage/dto/create-service-specific.dto.ts
+++ src/modules/servicesPackage/dto/create-service-specific.dto.ts
@@ -1,11 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateServiceSpecificDTO {
+  @ApiProperty({
+    example: '1',
+    description: 'The id of the plant season',
+  })
   plant_season_id: string;
 
+  @ApiProperty({
+    example: '1',
+    description: 'The id of the booking',
+  })
   booking_id: string;
 
+  @ApiProperty({
+    example: '1',
+    description: 'The id of the service package',
+  })
   service_package_id: string;
 
+  @ApiProperty({
+    example: 1,
+    description: 'The acreage of the plant',
+  })
   acreage_land: number;
 
+  @ApiProperty({
+    example: '2025-01-01',
+    description: 'The start time of the service',
+  })
   time_start: Date;
 }
