83785359889f208032b62e1b372989d9ea9a7715
HanTG <trinhhan255@gmail.com>
2024-11-18 12:49:45 +0700

    refactor: remove unused controller methods, implement IDinariesService interface, and enhance CreateDinaryDto with validation

--- src/modules/dinaries/dinaries.controller.ts
+++ src/modules/dinaries/dinaries.controller.ts
@@ -17,28 +17,5 @@ import { ApiTags } from '@nestjs/swagger';
 export class DinariesController {
   constructor(private readonly dinariesService: DinariesService) {}
 
-  @Post()
-  create(@Body() createDinaryDto: CreateDinaryDto) {
-    return this.dinariesService.create(createDinaryDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.dinariesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.dinariesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDinaryDto: UpdateDinaryDto) {
-    return this.dinariesService.update(+id, updateDinaryDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.dinariesService.remove(+id);
-  }
+  
 }
--- src/modules/dinaries/dinaries.service.ts
+++ src/modules/dinaries/dinaries.service.ts
@@ -1,26 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateDinaryDto } from './dto/create-dinary.dto';
 import { UpdateDinaryDto } from './dto/update-dinary.dto';
+import { IDinariesService } from './interfaces/IDinariesService.interface';
+import { In, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DinaryStage } from './entities/dinaryStage.entity';
+import { DinaryImage } from './entities/DinaryImange.entity';
 
 @Injectable()
-export class DinariesService {
-  create(createDinaryDto: CreateDinaryDto) {
-    return 'This action adds a new dinary';
-  }
+export class DinariesService implements IDinariesService {
+  constructor(
+    @InjectRepository(DinaryStage)
+    private readonly dinariesStageRepo: Repository<DinaryStage>,
 
-  findAll() {
-    return `This action returns all dinaries`;
-  }
+    @InjectRepository(DinaryImage)
+    private readonly dinariesImageRepo: Repository<DinaryImage>,
+  ) {}
 
-  findOne(id: number) {
-    return `This action returns a #${id} dinary`;
-  }
+//   async createDinary(data: CreateDinaryDto): Promise<any> {
+//    //check if dinary stage exist
+//    const dinary_stage_exist = await this.dinariesStageRepo.findOne({
+//       where: { dinary_stage_id: data.dinary_stage_id },
+//     });
 
-  update(id: number, updateDinaryDto: UpdateDinaryDto) {
-    return `This action updates a #${id} dinary`;
-  }
 
-  remove(id: number) {
-    return `This action removes a #${id} dinary`;
-  }
+// }
 }
