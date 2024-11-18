be14e427b9618a4cdb191578a173bac47a8e1390
Huỳnh Chí Bảo <baohc110902@gmail.com>
2024-11-18 15:19:13 +0700

    handle get list noti

--- src/modules/notifications/notifications.service.ts
+++ src/modules/notifications/notifications.service.ts
@@ -5,6 +5,7 @@ import { Notification } from './entities/notification.entity';
 import { CreateNotificationDTO } from './dto/create-notification.dto';
 import { EventGateway } from 'src/sockets/event.gateway';
 import { SocketEvent } from 'src/sockets/types/socket-event.enum';
+import { PaginationParams } from 'src/common/decorations/types/pagination.type';
 
 @Injectable()
 export class NotificationsService {
@@ -26,5 +27,32 @@ export class NotificationsService {
       SocketEvent.notification,
     );
     return this.notificationRepository.save(notification);
+  }
+
+  async getListByUser(
+    userId: string,
+    pagination: PaginationParams,
+  ): Promise<any> {
+    try {
+      const [notifications, total_count] = await Promise.all([
+        this.notificationRepository.find({
+          where: { user_id: userId },
+          order: { created_at: 'DESC' },
+          skip: (pagination.page_index - 1) * pagination.page_size,
+          take: pagination.page_size,
+        }),
+        this.notificationRepository.count({ where: { user_id: userId } }),
+      ]);
+      // get total page
+      const total_page = Math.ceil(total_count / pagination.page_size);
+
+      return {
+        notifications,
+        pagination: {
+          ...pagination,
+          total_page,
+        },
+      };
+    } catch (error) {}
   }
 }
