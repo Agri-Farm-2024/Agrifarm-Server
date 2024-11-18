ecb64ca343daa11a84073d4f9ad088270be98431
HanTG <trinhhan255@gmail.com>
2024-11-18 13:43:31 +0700

    feat: implement authentication guard for createDinaryStage, enhance DinariesService with logging and error handling, and update IDinariesService interface

--- src/modules/dinaries/dinaries.controller.ts
+++ src/modules/dinaries/dinaries.controller.ts
@@ -6,16 +6,35 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { DinariesService } from './dinaries.service';
 import { CreateDinaryDto } from './dto/create-dinary.dto';
 import { UpdateDinaryDto } from './dto/update-dinary.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+import { User } from '../users/entities/user.entity';
+import { UserRole } from '../users/types/user-role.enum';
+import { Role } from 'discord.js';
+import { Roles } from 'src/common/decorations/role.decoration';
 
 @ApiTags('Dinary')
 @Controller('dinaries')
 export class DinariesController {
   constructor(private readonly dinariesService: DinariesService) {}
 
-  
+  @UseGuards(AuthGuard)
+  @Roles(UserRole.expert)
+  @Post('/:process_specific_stage_content_id')
+  createDinaryStage(
+    @Body() createDinaryDto: CreateDinaryDto,
+    @Param('process_specific_stage_content_id')
+    process_stage_content_id: string,
+  ) {
+    return this.dinariesService.createDinary(
+      createDinaryDto,
+      process_stage_content_id,
+    );
+  }
 }
--- src/modules/dinaries/interfaces/IDinariesService.interface.ts
+++ src/modules/dinaries/interfaces/IDinariesService.interface.ts
@@ -1,1 +1,9 @@
-export interface IDinariesService {}
+import { Payload } from 'src/modules/auths/types/payload.type';
+import { CreateDinaryDto } from '../dto/create-dinary.dto';
+
+export interface IDinariesService {
+  createDinary(
+    data: CreateDinaryDto,
+    process_stage_content_id: string,
+  ): Promise<any>;
+}
