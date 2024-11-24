938c38b3449c1ddca4d3ede2f9fff3cfecb7ec68
Huỳnh Chí Bảo <baohc110902@gmail.com>
2024-11-24 14:45:19 +0700

    feat: restore notification logic for pending contract extensions

--- src/modules/extends/extends.service.ts
+++ src/modules/extends/extends.service.ts
@@ -185,15 +185,15 @@ export class ExtendsService implements IExtendService {
           throw new BadRequestException('Extend is not pending contract');
         }
         // // send notification to user
-        // await this.notificationService.createNotification({
-        //   user_id: extend.booking_land.landrenter_id,
-        //   component_id: extend.extend_id,
-        //   content: NotificationContentEnum.pending_sign_extend(
-        //     extend.booking_land.land.name,
-        //   ),
-        //   type: NotificationType.extend,
-        //   title: NotificationTitleEnum.pending_sign_extend,
-        // });
+        await this.notificationService.createNotification({
+          user_id: extend.booking_land.landrenter_id,
+          component_id: extend.extend_id,
+          content: NotificationContentEnum.pending_sign_extend(
+            extend.booking_land.land.name,
+          ),
+          type: NotificationType.extend,
+          title: NotificationTitleEnum.pending_sign_extend,
+        });
       }
       // check condition update to pending payment
       if (data.status === ExtendStatus.pending_payment) {
